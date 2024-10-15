cfda0914f037669b6282214deea90fc585bf8038
Gesletten <graces8@illinois.edu>
2024-10-15 18:37:47 -0500

    Homepage Complete

--- Project/frontend/app/components/navbar.js
+++ Project/frontend/app/components/navbar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 import styles from "./navbar.module.css";
 
-
 const Navbar = () => {
   return (
 
--- Project/frontend/app/page.js
+++ Project/frontend/app/page.js
@@ -1,91 +1,43 @@
 import Image from "next/image";
+import userImg from './public/user.png';
+import documentImg from './public/document.png';
+import writingImg from './public/writing.png';
+import sendImg from './public/send.png';
 import styles from "./page.module.css";
-import Navbar from "./components/navbar.js";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20} />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16} />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16} />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16} />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className = {styles.page}>
+      <div className={styles.part1}>
+        <h1> 
+          A good life is a collection of happy memories.
+        </h1>
+        <h2>
+          -Denis Waitley
+        </h2>
+      </div>
+      <div className={styles.row}>
+      <div class={styles.column}>
+      <h2>Step 1</h2>
+      <p>Create <br></br> Person</p>
+      <Image alt="document" src={userImg} className={styles.center}/>
+      </div>
+      <div class={styles.column}>
+      <h2>Step 2</h2>
+      <p>Upload <br></br> File</p>
+      <Image alt="document" src={documentImg} className={styles.center}/>
+      </div>
+      <div class={styles.column}>
+      <h2>Step 3</h2>
+      <p>Add <br></br> Description</p>
+      <Image alt="document" src={writingImg} className={styles.center}/>
+      </div>
+      <div class={styles.column}>
+      <h2>Step 4</h2>
+      <p>Share <br></br> Memories</p>
+      <Image alt="document" src={sendImg} className={styles.center}/>
+      </div>
+      </div>
     </div>
   );
 }
