ebeef98139dc14c45a78a0213c483e0ad073b200
Gesletten <graces8@illinois.edu>
2024-10-15 20:03:40 -0500

    Logo and Website Name Added

--- Project/frontend/app/components/navbar.js
+++ Project/frontend/app/components/navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from "./navbar.module.css";
+import Image from "next/image";
+import LogoImg from "../public/Logo.png";
 
 const Navbar = () => {
   return (
 
 <nav className={styles.navbar}>
     <ul>
+    <Image alt="logo" src={LogoImg} className={styles.logoPic}/>
       <li className = {styles.logo}>
-          Website Title Here
+          Memory Mosaic
       </li>
       <li>
       <Link href="/" className= {styles.item}>
