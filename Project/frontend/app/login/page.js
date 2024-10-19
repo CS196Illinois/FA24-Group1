7123331b27bdfa90ca0af506968eea441a0da8a1
Gesletten <graces8@illinois.edu>
2024-10-19 00:35:47 -0500

    Added sign in dummy page

--- Project/frontend/app/login/page.js
+++ Project/frontend/app/login/page.js
@@ -1,8 +1,27 @@
+import styles from './page.module.css';
+import Image from "next/image";
+import LogoImg from "/public/Logo.png";
+import Link from 'next/link';
+
 export default function Login() {
     return (
-      <div>
-        <h1>Login</h1>
-        <p>This is the login page.</p>
+      <div className = {styles.photo}>
+        <div className = {styles.pad}>
+          <div className = {styles.box}>
+              <div className = {styles.logoContainer}>
+                <Image src={LogoImg} className = {styles.logo}/>
+                <h1 className = {styles.title}>Memory Mosaic</h1>
+              </div>
+              <h4 className = {styles.boxDes}> Username</h4>
+              <input type="text" placeholder="Enter Username" className={styles.textBox} required></input>
+              <h4 className = {styles.boxDes}> Password</h4>
+              <input type="text" placeholder="Enter Password" className={styles.textBox} required></input>
+              <Link href="login/empty" className={styles.signIn}>
+                Sign In
+              </Link>
+          </div>
+        </div>
       </div>
     );
-  }
+}
+
