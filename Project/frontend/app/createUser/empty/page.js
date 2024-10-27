7f92cd604f52194e70a309b1de47f04c3614b9bd
Colin Su <colinwithpower@gmail.com>
2024-10-27 14:43:31 -0500

    shell of create user page

---
+++ Project/frontend/app/createUser/empty/page.js
@@ -0,0 +1,8 @@
+export default function empty() {
+    return (
+      <div>
+        <h1>Nothing Yet</h1>
+        <p>This is to make it look like a create user page.</p>
+      </div>
+    );
+  }
