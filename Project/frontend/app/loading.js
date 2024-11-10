0ca122336feaf64c252d18a88a261aa9745572a3
Gesletten <graces8@illinois.edu>
2024-11-10 12:15:05 -0600

    loading screen

---
+++ Project/frontend/app/loading.js
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+function Loading() {
+    return <div className={styles.background}>
+        <p1 className = {styles.loading}> Loading... </p1>
+    </div> ;
+}
+
+export default Loading;
